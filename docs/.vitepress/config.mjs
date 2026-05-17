3d991447578cb2a15e89553a58d521d2e692d4b3
tiago <70700766+tiagozip@users.noreply.github.com>
2026-05-17 13:42:30 +0100

    docs: update twitter link

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -272,7 +272,7 @@ export default withMermaid({
 
     socialLinks: [
       { icon: "github", link: "https://github.com/tiagozip/cap" },
-      { icon: "twitter", link: "https://x.com/0xtiago_" },
+      { icon: "twitter", link: "https://x.com/tiagozip_" },
     ],
 
     footer: {
