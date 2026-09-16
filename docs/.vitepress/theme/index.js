3999aeff015b1880ae06f3e472f256e6d9747a9e
tiago <70700766+tiagozip@users.noreply.github.com>
2026-09-16 17:14:27 +0100

    fix(docs): remove unshipped theme imports and dead strings

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,15 +5,11 @@ import Demo from "./components/Demo.vue";
 import DemoReply from "./components/DemoReply.vue";
 import DemoTabs from "./components/DemoTabs.vue";
 import HomeV2 from "./components/HomeV2.vue";
-import HomeV3 from "./components/HomeV3.vue";
-import Layout from "./Layout.vue";
 import "./style.css";
-import "./docs.css";
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
-  Layout,
   enhanceApp({ app, router, siteData }) {
     if (typeof window !== "undefined") {
       const known = Object.keys(siteData.value.locales).filter((k) => k !== "root");
@@ -33,6 +29,5 @@ export default {
     app.component("DemoReply", DemoReply);
     app.component("DemoTabs", DemoTabs);
     app.component("HomeV2", HomeV2);
-    app.component("HomeV3", HomeV3);
   },
 };
